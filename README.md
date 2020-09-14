469439d7bcfa66df43b2980a8c3adc14eba3d991
iAmMichaelConnor <michael.connor@uk.ey.com>
2020-09-14 18:45:48 +0100

    feat: initial gn functionality

--- README.md
+++ README.md
@@ -1,1 +1,226 @@
 # general-number
+
+A library for easily converting a number to various types.
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+
+- [Install](#install)
+- [Usage](#usage)
+  - [new GN()](#new-gn)
+  - [generalise()](#generalise)
+- [Developer](#developer)
+  - [Test](#test)
+  - [Test with yalc](#test-with-yalc)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+## Install
+
+In your NodeJS project:  
+`npm i -s general-number`
+
+## Usage
+
+(See `./__tests__/general-number.js` for a comprehensive set of GN conversion commands).
+
+### new GN()
+
+If passed a some value, `new GN()` will infer the type of that value, and can return conversions of that value through custom getters:
+
+```js
+import { GN } from 'general-number';
+
+const myOriginalNumber = '0x12345678';
+
+const myGN = new GN(myOriginalNumber);
+
+console.log(myGN.hex());
+// should return '0x12345678'
+
+console.log(myGN.hex(32));
+// should return as 32 bytes: '0x0000000000000000000000000000000000000000000000000000000012345678'
+
+console.log(myGN.hex(2));
+// should return as 2 bytes: '0x5678'
+
+console.log(myGN.hex(8, 2));
+// should return as 2 bytes, padded to 8 bytes: '0x0000000000005678'
+
+console.log(myGN.decimal);
+// should return as a decimal string: '305419896'
+
+console.log(myGN.integer);
+// synonym for 'decimal': '305419896'
+
+console.log(myGN.number);
+// should return as a js number: 305419896
+
+console.log(myGN.bigInt);
+// should return as a js BigInt: BigInt(myOriginalNumber)
+
+console.log(myGN.binary);
+// should return as a binary string: '10010001101000101011001111000'
+
+console.log(myGN.binaryArray);
+// should return as a binary array: '[1,0,0,1,0,0,0,1,1,0,1,0,0,0,1,0,1,0,1,1,0,0,1,1,1,1,0,0,0]'
+
+console.log(myGN.field(256, false));
+// should return as a finite field element: (myGN % 256): '120'
+
+console.log(myGN.field(256));
+// should throw an error (myGN is larger than the requested modulus 256)
+
+console.log(myGN.limbs(16));
+// should return as 16-bit decimal limbs: ['4660', '22136']
+
+console.log(myGN.limbs(16, 4));
+// should return as 4x 16-bit decimal limbs: ['0', '0', '4660', '22136']
+
+console.log(myGN.limbs(16, 4, 'hex'));
+// should return as 4x 16-bit hex limbs: ['0x0', '0x0', '0x1234', '0x5678']
+
+console.log(myGN.ascii);
+// should return as an ascii string: '#Eg'
+
+console.log(myGN.utf8);
+// should return as an ascii string: 'x'
+
+```
+
+In some circumstances, you may need to be specific about the type you're passing to `new GN()`. In particular, when passing a `binary` value.
+
+
+```js
+import { GN } from 'general-number';
+
+const myOriginalNumber = '10010001101000101011001111000';
+
+// const myGN = new GN(myOriginalNumber); // BAD!!! - this would infer the 'type' as decimal!!!
+
+const myGN = new GN(myOriginalNumber, 'binary');
+
+console.log(myGN.hex());
+// should return '0x12345678'
+
+```
+
+### generalise()
+
+Generalise is a powerful function for generalising _all_ values within an object or an array.
+
+Americans: don't worry, `generalize` works too!
+
+```js
+import { generalise } from 'general-number';
+
+
+// OBJECTS:
+
+const myObj = {
+  key1: '0xf198e3403bdda3a0',
+  key2: '17408914224622445472',
+};
+
+const myGeneralisedObj = generalise(myObj);
+
+console.log(myGeneralisedObj.key1.hex());
+// should return as hex: '0xf198e3403bdda3a0'
+
+console.log(myGeneralisedObj.key2.hex());
+// should return as hex: '0xf198e3403bdda3a0'
+
+console.log(myGeneralisedObj.all.hex());
+// should return the original object's structure, but all values as hex:
+// {
+//   key1: '0xf198e3403bdda3a0',
+//   key2: '0xf198e3403bdda3a0',
+// }
+
+
+// ARRAYS:
+
+const myArr = [
+  '0xf198e3403bdda3a0',
+  '17408914224622445472',
+];
+
+const myGeneralisedArr = generalise(myArr);
+
+console.log(myGeneralisedObj[0].decimal);
+// should return as hex: '17408914224622445472'
+
+console.log(myGeneralisedObj[1].decimal);
+// should return as hex: '17408914224622445472'
+
+console.log(myGeneralisedObj.all.decimal);
+// should return the original array's structure, but all values as hex:
+// [
+//   '17408914224622445472',
+//   '17408914224622445472',
+// ]
+```
+
+Notice: `.all` is a custom property given to generalised objects (and arrays), which enables developers to 'get' _all_ values of the object (or array) as the same 'type' (e.g. as `hex` in the above example).
+
+`generalise()` can also be used on *nested* objects or arrays (including arrays of objects or objects of arrays). Each sub-object will be given its own `.all` property.
+
+`generalise()` can also be used instead of `new GN()` for simple values in all cases (except when passing a value of type `limbs`). So in the [first example](#new-gn), `const myGN = generalise(myOriginalNumber);` would have been equally valid sytax.
+
+## Developer
+
+### Test
+
+Clone the repo. `npm i`  
+`npm test`
+
+### Test with yalc
+
+If you've made local changes to this repo, and would like to test whether those (unpublished) changes will work with some dependent zkp appliation (zApp)...
+
+...then you’ll need to install your local, ‘branched’ version of `general-number` in your zApp.
+
+```sh
+cd path/to/general-number/
+yalc publish
+```
+
+You should see something like "`@eyblockchain/general-number@0.0.0-3df45b8c published in store.`". Notice the ‘signature’ `3df45b8c` .
+
+```sh
+cd path/to/your/zApp/
+```
+
+Remove the package-lock.json and the node_modules from your zApp's root (if they exist on your machine).
+
+Then:
+
+```sh
+yalc add @eyblockchain/general-number
+```
+
+You’ll see that this has ‘swapped-in’ the ‘published’ (yalc version) of `general-number` in the `package.json`. It’s also created `.yalc.lock` (which shows that you’ve replaced the ‘proper’ npm package of `general-number` with your ‘yalc version’ (see the `signature` field in this file, which should match the signature from earlier)).
+
+Now install node modules:
+
+```sh
+npm i
+```
+
+If your zApp runs in a container, then you might need to also edit its dockerfile:
+
+Change a line in `<your zApp>/Dockerfile` from `RUN npm ci` to `RUN npm install`.
+
+Add a line in `<your zApp>/docker-compose.yml`:
+
+```yaml
+yourService:
+  build:
+    context: .
+    dockerfile: Dockerfile
+volumes:
+  - ./.yalc:/app/.yalc # <<< ADD THIS LINE (or something similar)!!!
+```
+
+When you're happy that your local changes to `general-number` work and you wish to create a PR, you MUST remove any references to `yalc`, or the PR shouldn't be accepted.
---
+++ __tests__/general-number.js
@@ -0,0 +1,342 @@
+const { hexToBinArray, hexToBytes } = require('nightlite-utils');
+const { generalise, GN, GO, stitchLimbs } = require('../index');
+
+const dec = '17408914224622445472';
+const hex = '0xf198e3403bdda3a0';
+const bin = '1111000110011000111000110100000000111011110111011010001110100000';
+
+const object = {
+  dec,
+  hex,
+};
+
+const array = [dec, hex];
+
+const arrayOfObjects = [object, object];
+const objectOfArrays = { key0: array, key1: array };
+
+function deepEqual(x, y) {
+  const ok = Object.keys;
+  const tx = typeof x;
+  const ty = typeof y;
+  return x && y && tx === 'object' && tx === ty
+    ? ok(x).length === ok(y).length && ok(x).every(key => deepEqual(x[key], y[key]))
+    : x === y;
+}
+
+describe('GeneralNumber class', () => {
+  test('Create GN from hex', () => {
+    const gn = new GN(hex);
+
+    expect(gn.binary).toEqual(bin);
+    expect(gn.binaryArray).toEqual(hexToBinArray(hex));
+    expect(gn.bytes).toEqual(hexToBytes(hex));
+    expect(gn.decimal).toEqual(dec);
+    expect(gn.integer).toEqual(dec);
+    expect(() => gn.number).toThrow(); // number too large for native JS
+    expect(gn.bigInt).toEqual(BigInt(dec));
+
+    expect(gn.hex()).toEqual(hex);
+    expect(gn.hex(32)).toEqual(
+      '0x000000000000000000000000000000000000000000000000f198e3403bdda3a0',
+    );
+    expect(gn.hex(32, 4)).toEqual(
+      '0x000000000000000000000000000000000000000000000000000000003bdda3a0',
+    );
+    expect(gn.hex(4)).toEqual('0x3bdda3a0');
+    expect(() => gn.hex(4, 5)).toThrow();
+
+    expect(() => gn.limbs()).toThrow();
+
+    expect(gn.limbs(32)).toEqual(['4053328704', '1004381088']);
+    expect(gn.limbs(32, 4)).toEqual(['0', '0', '4053328704', '1004381088']);
+    expect(gn.limbs(32, 1)).toEqual(['1004381088']);
+    expect(() => gn.limbs(32, 1, 'decimal', true)).toThrow(); // 'true' throws errors
+
+    expect(gn.limbs(32, undefined, 'hex')).toEqual(['0xf198e340', '0x3bdda3a0']);
+    expect(gn.limbs(32, 4, 'hex')).toEqual(['0x0', '0x0', '0xf198e340', '0x3bdda3a0']);
+    expect(gn.limbs(32, 1, 'hex')).toEqual(['0x3bdda3a0']);
+    expect(() => gn.limbs(32, 1, 'hex', true)).toThrow(); // 'true' throws errors
+
+    expect(gn.field(256, false)).toEqual('160'); // 'false' allows overflow of the modulus, and results in modulo reduction, returning (gn % 256)
+    expect(() => gn.field(256)).toThrow(); // defaults to preventing overflow of the field modulus (256)
+  });
+
+  test('Create GN from an integer', () => {
+    const gn = new GN(dec, 'integer');
+
+    expect(gn.binary).toEqual(bin);
+    expect(gn.binaryArray).toEqual(hexToBinArray(hex));
+    expect(gn.bytes).toEqual(hexToBytes(hex));
+    expect(gn.decimal).toEqual(dec);
+    expect(gn.integer).toEqual(dec);
+    expect(() => gn.number).toThrow(); // number too large for native JS
+    expect(gn.bigInt).toEqual(BigInt(dec));
+
+    expect(gn.hex()).toEqual(hex);
+    expect(gn.hex(32)).toEqual(
+      '0x000000000000000000000000000000000000000000000000f198e3403bdda3a0',
+    );
+    expect(gn.hex(32, 4)).toEqual(
+      '0x000000000000000000000000000000000000000000000000000000003bdda3a0',
+    );
+    expect(gn.hex(4)).toEqual('0x3bdda3a0');
+    expect(() => gn.hex(4, 5)).toThrow();
+
+    expect(() => gn.limbs()).toThrow();
+
+    expect(gn.limbs(32)).toEqual(['4053328704', '1004381088']);
+    expect(gn.limbs(32, 4)).toEqual(['0', '0', '4053328704', '1004381088']);
+    expect(gn.limbs(32, 1)).toEqual(['1004381088']);
+    expect(() => gn.limbs(32, 1, 'decimal', true)).toThrow(); // 'true' throws errors
+
+    expect(gn.limbs(32, undefined, 'hex')).toEqual(['0xf198e340', '0x3bdda3a0']);
+    expect(gn.limbs(32, 4, 'hex')).toEqual(['0x0', '0x0', '0xf198e340', '0x3bdda3a0']);
+    expect(gn.limbs(32, 1, 'hex')).toEqual(['0x3bdda3a0']);
+    expect(() => gn.limbs(32, 1, 'hex', true)).toThrow(); // 'true' throws errors
+  });
+
+  test('Create GN from a number', () => {
+    const gn = new GN(1234, 'number');
+
+    expect(gn.binary).toEqual('10011010010');
+    expect(gn.binaryArray).toEqual(hexToBinArray('0x4d2'));
+    expect(gn.bytes).toEqual(hexToBytes('0x4d2'));
+    expect(gn.decimal).toEqual('1234');
+    expect(gn.integer).toEqual('1234');
+    expect(gn.number).toEqual(1234); // number too large for native JS
+    expect(gn.bigInt).toEqual(BigInt(1234));
+
+    // no need to check the other getters; they'll be similar to 'integer' getters.
+  });
+
+  test('Create GN from a BigInt', () => {
+    const bigInt = BigInt(1234);
+    const gn = new GN(bigInt);
+
+    expect(gn.binary).toEqual('10011010010');
+    expect(gn.binaryArray).toEqual(hexToBinArray('0x4d2'));
+    expect(gn.bytes).toEqual(hexToBytes('0x4d2'));
+    expect(gn.decimal).toEqual('1234');
+    expect(gn.integer).toEqual('1234');
+    expect(gn.number).toEqual(1234); // number too large for native JS
+    expect(gn.bigInt).toEqual(BigInt(1234));
+
+    // no need to check the other getters; they'll be similar to 'integer' getters.
+  });
+
+  test('Create GN from ascii', () => {
+    const gn = new GN('hello', 'ascii');
+    expect(gn.hex()).toEqual('0x68656c6c6f');
+    expect(gn.binary).toEqual('110100001100101011011000110110001101111');
+    expect(gn.binaryArray).toEqual(hexToBinArray(gn.hex()));
+    expect(gn.bytes).toEqual(hexToBytes(gn.hex()));
+    expect(gn.integer).toEqual('448378203247');
+    expect(gn.limbs(32)).toEqual(['104', '1701604463']);
+    expect(gn.limbs(32, 4)).toEqual(['0', '0', '104', '1701604463']);
+  });
+
+  test('Create GN from utf8', () => {
+    const gn = new GN('hello', 'utf8');
+    expect(gn.hex()).toEqual('0x68656c6c6f');
+    expect(gn.binary).toEqual('110100001100101011011000110110001101111');
+    expect(gn.binaryArray).toEqual(hexToBinArray(gn.hex()));
+    expect(gn.bytes).toEqual(hexToBytes(gn.hex()));
+    expect(gn.integer).toEqual('448378203247');
+    expect(gn.limbs(32)).toEqual(['104', '1701604463']);
+    expect(gn.limbs(32, 4)).toEqual(['0', '0', '104', '1701604463']);
+  });
+});
+
+describe('generalise function', () => {
+  test('Create GN from hex', () => {
+    const gn1 = generalise(hex);
+    const gn2 = new GN(hex);
+    expect(deepEqual(gn1, gn2)).toEqual(true);
+  });
+
+  test('Create GN from an integer', () => {
+    const gn1 = generalise(dec);
+    const gn2 = new GN(dec);
+    expect(deepEqual(gn1, gn2)).toEqual(true);
+  });
+
+  test('generalise an object', () => {
+    const obj = generalise(object);
+    const gnHex = new GN(hex);
+    const gnDec = new GN(dec);
+    expect(deepEqual(obj.hex, gnHex)).toEqual(true);
+    expect(deepEqual(obj.dec, gnDec)).toEqual(true);
+  });
+
+  test('generalise an array', () => {
+    const arr = generalise(array);
+    const gnHex = new GN(hex);
+    const gnDec = new GN(dec);
+    expect(deepEqual(arr[0], gnDec)).toEqual(true);
+    expect(deepEqual(arr[1], gnHex)).toEqual(true);
+  });
+});
+
+describe('the ".all" property', () => {
+  test('Call ".all" on an array', () => {
+    const arr = generalise(array);
+    const gn = new GN(hex);
+    const goArr = new GO(arr);
+    expect(deepEqual(arr.all, goArr)).toEqual(true);
+    expect(arr.all.hex()).toEqual([hex, hex]);
+    expect(arr.all.decimal).toEqual([dec, dec]);
+    expect(arr.all.binary).toEqual([bin, bin]);
+    expect(arr.all.hex(2)).toEqual([gn.hex(2), gn.hex(2)]);
+    expect(arr.all.limbs(32)).toEqual([gn.limbs(32), gn.limbs(32)]);
+    expect(arr.all.limbs(32, 4, 'hex')).toEqual([gn.limbs(32, 4, 'hex'), gn.limbs(32, 4, 'hex')]);
+  });
+
+  test('Call ".all" on an object', () => {
+    const obj = generalise(object);
+    const gn = new GN(hex);
+    const go = new GO(obj);
+    expect(deepEqual(obj.all, go)).toEqual(true);
+    expect(
+      deepEqual(obj.all.hex(), {
+        dec: hex,
+        hex,
+      }),
+    ).toEqual(true);
+    expect(
+      deepEqual(obj.all.decimal, {
+        dec,
+        hex: dec,
+      }),
+    ).toEqual(true);
+    expect(
+      deepEqual(obj.all.binary, {
+        dec: bin,
+        hex: bin,
+      }),
+    ).toEqual(true);
+    expect(
+      deepEqual(obj.all.hex(2), {
+        dec: gn.hex(2),
+        hex: gn.hex(2),
+      }),
+    ).toEqual(true);
+    expect(
+      deepEqual(obj.all.limbs(32, 4, 'hex'), {
+        dec: gn.limbs(32, 4, 'hex'),
+        hex: gn.limbs(32, 4, 'hex'),
+      }),
+    ).toEqual(true);
+  });
+
+  test('Call ".all" on an array of objects', () => {
+    const arr = generalise(arrayOfObjects);
+    const go = new GO(arrayOfObjects);
+    const gn = new GN(hex);
+    expect(deepEqual(arr.all, go)).toEqual(true);
+    expect(
+      deepEqual(arr.all.hex(), [
+        {
+          dec: hex,
+          hex,
+        },
+        {
+          dec: hex,
+          hex,
+        },
+      ]),
+    ).toEqual(true);
+    expect(
+      deepEqual(arr.all.decimal, [
+        {
+          dec,
+          hex: dec,
+        },
+        {
+          dec,
+          hex: dec,
+        },
+      ]),
+    ).toEqual(true);
+    expect(
+      deepEqual(arr.all.binary, [
+        {
+          dec: bin,
+          hex: bin,
+        },
+        {
+          dec: bin,
+          hex: bin,
+        },
+      ]),
+    ).toEqual(true);
+    expect(
+      deepEqual(arr.all.hex(2), [
+        {
+          dec: gn.hex(2),
+          hex: gn.hex(2),
+        },
+        {
+          dec: gn.hex(2),
+          hex: gn.hex(2),
+        },
+      ]),
+    ).toEqual(true);
+    expect(
+      deepEqual(arr.all.limbs(32, 4, 'hex'), [
+        {
+          dec: gn.limbs(32, 4, 'hex'),
+          hex: gn.limbs(32, 4, 'hex'),
+        },
+        {
+          dec: gn.limbs(32, 4, 'hex'),
+          hex: gn.limbs(32, 4, 'hex'),
+        },
+      ]),
+    ).toEqual(true);
+  });
+
+  test('Call ".all" on an object of arrays', () => {
+    const obj = generalise(objectOfArrays);
+    const go = new GO(objectOfArrays);
+    const gn = new GN(hex);
+    expect(deepEqual(obj.all, go)).toEqual(true);
+    expect(obj.all.hex()).toEqual({
+      key0: [hex, hex],
+      key1: [hex, hex],
+    });
+    expect(obj.all.decimal).toEqual({
+      key0: [dec, dec],
+      key1: [dec, dec],
+    });
+    expect(obj.all.binary).toEqual({
+      key0: [bin, bin],
+      key1: [bin, bin],
+    });
+    expect(obj.all.hex(2)).toEqual({
+      key0: [gn.hex(2), gn.hex(2)],
+      key1: [gn.hex(2), gn.hex(2)],
+    });
+    expect(obj.all.limbs(32)).toEqual({
+      key0: [gn.limbs(32), gn.limbs(32)],
+      key1: [gn.limbs(32), gn.limbs(32)],
+    });
+    expect(obj.all.limbs(32, 4, 'hex')).toEqual({
+      key0: [gn.limbs(32, 4, 'hex'), gn.limbs(32, 4, 'hex')],
+      key1: [gn.limbs(32, 4, 'hex'), gn.limbs(32, 4, 'hex')],
+    });
+  });
+
+  test('Call ".all" on a GN', () => {
+    const gn = new GN(hex);
+    expect(deepEqual(gn.all, gn)).toEqual(true);
+  });
+});
+
+describe('stitchLimbs function', () => {
+  test('Create a hex string from limbs', () => {
+    const limbs = [1, 10, 15];
+    const hexStr = stitchLimbs(limbs, 16);
+    expect(hexStr).toEqual('0x1000a000f');
+  });
+});

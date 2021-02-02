03499f77247c7d6f1eb35dcd8b86bd1e4ba636a8
ilyasridhuan <ilyasridhuan@gmail.com>
2021-02-02 20:59:24 +0800

    Fixed typo that results in dangling reference.Objects passed into inferType (although that is unlikely to happen) interpreted as ASCII

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const inferType = value => {
     // ensure all elements of the array are of the same type:
     if (value.map(inferType).every((val, i, arr) => val === arr[0])) return 'limbs'; // infer arrays whcich contain elements of the same type as 'limbs'
   }
-  if (typeof thing === 'object') {
+  if (typeof value === 'object') {
     throw new Error(
       `Cannot construct a new GeneralNumber from an object (unless it's a 'BigInt' or 'limbs' array). Received ${value}. Try using the 'generalise()' function on this object instead.`,
     );
